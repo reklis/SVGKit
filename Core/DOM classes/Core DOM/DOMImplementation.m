55345386f67235d3228c3955aa34d8a5ca581500
adamgit <adam.m.s.martin@gmail.com>
2012-05-25 02:53:20 +0100

    Xcode is giving runtime warnings of a major problem - Apple named one of their private, global classes "DOMImplementation", which clashes with the DOM class of the same name. Can we re-use Apple's? I have no idea...

--- Core/DOM classes/Core DOM/DOMImplementation.m
+++ Core/DOM classes/Core DOM/DOMImplementation.m
@@ -12,6 +12,8 @@
 
 -(BOOL) hasFeature:(NSString*) feature version:(NSString*) version
 {
+	NSAssert( FALSE, @"Apple has made a private class with the same name as this class - we have to rename it because ObjectiveC sucks, and Apple used a foolish name, so we CANNOT adhere to the spec now" );
+	
 	NSAssert( FALSE, @"Not implemented yet" );
 	return FALSE;
 }
@@ -19,6 +21,8 @@
 // Introduced in DOM Level 2:
 -(DocumentType*) createDocumentType:(NSString*) qualifiedName publicId:(NSString*) publicId systemId:(NSString*) systemId
 {
+	NSAssert( FALSE, @"Apple has made a private class with the same name as this class - we have to rename it because ObjectiveC sucks, and Apple used a foolish name, so we CANNOT adhere to the spec now" );
+	
 	NSAssert( FALSE, @"Not implemented yet" );
 	return nil;
 }
@@ -27,6 +31,8 @@
 // Introduced in DOM Level 2:
 -(Document*) createDocument:(NSString*) namespaceURI qualifiedName:(NSString*) qualifiedName doctype:(DocumentType*) doctype
 {
+	NSAssert( FALSE, @"Apple has made a private class with the same name as this class - we have to rename it because ObjectiveC sucks, and Apple used a foolish name, so we CANNOT adhere to the spec now" );
+	
 	NSAssert( FALSE, @"Not implemented yet" );
 	return nil;
 }
